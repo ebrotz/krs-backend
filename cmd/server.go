26edce828d12fab616a9dcb868b066c430f68688
Edward Brotz <edward.brotz@gmail.com>
2026-01-06 16:01:45 -0800

    Write to stdout that the server is starting

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/ebrotz/krs-backend/internal/places"
@@ -9,5 +10,6 @@ import (
 func main() {
 	m := http.NewServeMux()
 	m.Handle("/", places.NewPlaceHandler())
+	fmt.Println("Starting krs-backend on port 8080")
 	_ = http.ListenAndServe(":8080", m)
 }
